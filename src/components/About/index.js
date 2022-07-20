8886f3dfdefa86579445a8465e42715b3662611f
Abstrack <Armon_ah05@outlook.com>
2022-07-19 23:09:32 -0400

    started working on gallery/photo, updated nav with lift state, website title correspond to appropriate gallery seection

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import coverImage from '../../assets/cover/cover-image.jpg';
 
-const About = ()=>{
+const About = () => {
     return(
         <section className='my-5'>
             <h1 id='about'>Who am I?</h1>
--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -1,54 +1,52 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { capitalizeFirstLetter } from "../../utils/helpers";
 
-const categories = [
-  {
-      name: 'Commercial',
-      description: 'Photos of grocery stores, food trucks, and other commercial projects',
-  },
-  {
-      name: 'Portraits',
-      description: 'Portraits of people in my life',
-  },
-  {
-      name: 'Landscape',
-      description: 'Fields, farmhouses, waterfalls, and the beauty of nature',
-  },
-];
+const Nav = (props) => {
 
-function categorySelected(name) {
-  console.log(`${name} clicked`);
-  // console.log('You clicked a Nav link!')
-};
+const {
+  categories = [],
+  setCurrentCategory,
+  currentCategory,
+} = props;
 
-const Nav = () => {
- 
+
+// updates the website title to the current nav tab 'commericial, portraits, landscape'
+useEffect(() => {
+  document.title = capitalizeFirstLetter(currentCategory.name)
+}, [currentCategory]); //directs the hook to re-render the component on changes to the value of this state
+
   return (
     <header className="flex-row px-1">
       <h2>
-        <a data-testid='link' href="/" className="flex-row px-1 my-1">
-          <span role="img" aria-label="camera"> 📸</span>Oh Snap!
+        <a data-testid="link" href="/" className="flex-row px-1 my-1">
+          <span role="img" aria-label="camera">
+            {" "}
+            📸
+            {" "}
+          </span>
+          Oh Snap!
         </a>
       </h2>
       <nav>
-        <ul className='flex-row'>
-            <li className='mx-2'>
-                <a data-testid='about' href='#about'>
-                    About Me
-                </a>
+        <ul className="flex-row">
+          <li className="mx-2">
+            <a data-testid="about" href="#about">
+              About Me
+            </a>
+          </li>
+          <li>
+            <span>Contact</span>
+          </li>
+          {categories.map((category) => (
+            // Whenever we map over anything in JSX, the outermost element
+            // must have a key attribute that's set to be something unique.
+            // This helps React keep track of items in the virtual DOM.
+            <li className={`mx-1 ${currentCategory.name === category.name && 'navActive'}`} key={category.name}>
+              <span onClick={() => setCurrentCategory(category)}>
+                {capitalizeFirstLetter(category.name)}
+              </span>
             </li>
-            <li>
-                <span>Contact</span>
-            </li>
-            {categories.map((category) => (
-                // Whenever we map over anything in JSX, the outermost element 
-                // must have a key attribute that's set to be something unique. 
-                // This helps React keep track of items in the virtual DOM.
-                <li className='mx-2' key={category.name} >
-                     <span onClick={() => categorySelected(category.name)} >
-                      {category.name}
-                      </span>
-                </li>
-            ))}
+          ))}
         </ul>
       </nav>
     </header>
