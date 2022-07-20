ae321261ce05719ee14c4fda2ad9dceea2b5d449
Abstrack <Armon_ah05@outlook.com>
2022-07-20 13:29:39 -0400

    added validation to contact form

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -1,20 +1,45 @@
 import React, { useState } from "react";
+import { validateEmail } from '../../utils/helpers'
 
 // When that form data is maintained by the state of the component, we call it a controlled component. When the data is retrieved, then submitted directly from the DOM, we call it an uncontrolled component.
 const ContactForm = () => {
+  const [errorMessage, setErrorMessage] = useState(' ');
   const [formState, setFormState] = useState({ name: '', email: '', message: '' });
   const { name, email, message } = formState;
 
   const handleChange = (e) => {
-    setFormState({...formState, [e.target.name]: e.target.value })
-    // The name property of target in the preceding expression actually refers to the name attribute of the form element. This attribute value matches the property names of formState (name, email, and message) 
-    // and allows us to use [ ] to create dynamic property names.
+
+    if(e.target.name === 'email') {
+      const isValid = validateEmail(e.target.value);
+      console.log(isValid);
+        if(!isValid) {
+          setErrorMessage('Your email is invalid!');
+        }  else {
+          setErrorMessage('');
+        }
+    } 
+    else {
+      if(!e.target.value.length) {
+        setErrorMessage(`${e.target.name} is required`);
+      } else {
+        setErrorMessage(' ');
+      }
+    }
+
+    if (!errorMessage) { // The preceding conditional statement only allows the state to update with the user input if there is no error message, which is the correct logic.
+      setFormState({...formState, [e.target.name]: e.target.value })
+      // The name property of target in the preceding expression actually refers to the name attribute of the form element. This attribute value matches the property names of formState (name, email, and message) 
+      // and allows us to use [ ] to create dynamic property names.
+    }
+    // console.log('errorMessage', errorMessage);
+
   }
   // console.log(formState);
 
+  
   const handleSubmit = (e) => {
     e.preventDefault();
-    console.log(formState);
+    // console.log(formState);
   };
   
   return (
@@ -29,7 +54,7 @@ const ContactForm = () => {
             type="text"
             name="name"
             defaultValue={name}
-            onChange={handleChange}
+            onBlur={handleChange}
           />
           {/* This function (onChange={handleChange}) will sync the internal state of the component formState with the 
           user input from the DOM. The onChange event listener will ensure that the handleChange 
@@ -37,12 +62,18 @@ const ContactForm = () => {
         </div>
         <div>
           <label htmlFor="email">Email</label>
-          <input type="email" name="email" defaultValue={email} onChange={handleChange} />
+          <input type="email" name="email" defaultValue={email} onBlur={handleChange} />
         </div>
         <div>
           <label htmlFor="message">Message</label>
-          <textarea name="message" rows="5" defaultValue={message} onChange={handleChange} />
+          <textarea name="message" rows="5" defaultValue={message} onBlur={handleChange} />
+          {/* The onBlur attribute will fire the event once the user has changed focus from the input field, thus allowing the user to finish their entry before validating their input. */}
         </div>
+        {errorMessage && (
+          <div>
+            <p className="error-text" >{errorMessage}</p>
+          </div>
+        )}
         <button type="submit">Submit</button>
       </form>
     </section>
